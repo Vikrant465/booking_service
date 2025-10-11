6aae55906b68ce02d875fc7f4753aa14b2f36aeb
vikrant <viksing465@gmail.com>
2025-10-11 14:42:04 +0530

    env

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,9 +6,9 @@ import { HeroUIProvider } from '@heroui/react'
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
-      {/* <SessionProvider> */}
+      
         {children}
-      {/* </SessionProvider> */}
+      
     </HeroUIProvider>
   )
 }
